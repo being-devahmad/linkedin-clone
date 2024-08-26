73696936a6dd1d752c2b3871e24f4ca8c47b5c81
being-devahmad <devahmad41@gmail.com>
2024-08-27 00:55:02 +0500

    database connection commit

---
+++ src/lib/db.ts
@@ -0,0 +1,18 @@
+import mongoose, { Connection } from "mongoose"
+
+let isConnected: Connection | boolean = false
+
+const dbConnect = async () => {
+    if (isConnected) {
+        console.log("Database already connected....")
+        return isConnected
+    }
+    try {
+        const res = await mongoose.connect(process.env.MONGO_URL!)
+        isConnected = res.connection
+        console.log("Database connected successfully....")
+        return isConnected
+    } catch (error) {
+        console.log(error)
+    }
+}
